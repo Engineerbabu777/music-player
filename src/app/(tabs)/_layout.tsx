18b7190c1eae836cdeecd7543b1384cc5c3f7366
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-05-03 16:01:23 +0500

    add: playlists initial screen

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -4,7 +4,7 @@ const TabsNavigation = () => {
 	return (
 		<Tabs>
 			<Tabs.Screen name="favorites" />
-			{/* <Tabs.Screen name="playlists" /> */}
+			<Tabs.Screen name="playlists" />
 			<Tabs.Screen name="(songs)" />
 			{/* <Tabs.Screen name="artists" /> */}
 		</Tabs>
